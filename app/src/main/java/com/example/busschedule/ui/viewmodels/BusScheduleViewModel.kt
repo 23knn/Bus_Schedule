9fbe74fccb95a46346caf8e1c9cc77c9284768e3
nguch <kelvin.1.ngure@gmail.com>
2023-04-15 18:14:07 +0300

    created the View model with its companion object and Factory. also connected the view model to the app container and Base override Application

--- app/src/main/java/com/example/busschedule/ui/viewmodels/BusScheduleViewModel.kt
+++ app/src/main/java/com/example/busschedule/ui/viewmodels/BusScheduleViewModel.kt
@@ -2,10 +2,22 @@ package com.example.busschedule.ui.viewmodels
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory.Companion.APPLICATION_KEY
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.busschedule.BusScheduleApplication
 import com.example.busschedule.data.BusScheduleRepository
 
 class BusScheduleViewModel(val repository: BusScheduleRepository): ViewModel() {
 
+
+    companion object {
+        val Factory: ViewModelProvider.Factory = viewModelFactory {
+            initializer {
+                val app = (this[APPLICATION_KEY] as BusScheduleApplication) // get the app
+                val repo = app.container.busScheduleRepository// get the repo. our VM needs it
+                BusScheduleViewModel(repository = repo)
+            }
+        }
+    }
 }
